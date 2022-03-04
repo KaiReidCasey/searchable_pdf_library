542e21786c6936a9d25b81e791e77baebea57547
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2022-03-04 02:17:43 -0500

    Got Python script to read search terms from CSV

---
+++ resources/find_terms_in_file.py
@@ -0,0 +1,11 @@
+search_term_file = open("search_terms.csv", "r")
+# https://pdfgrep.org/ may be helpful for searching the PDF
+# pdf_to_search = open("pdfs/searchable_pdf_000.pdf", "r")
+
+search_terms = search_term_file.readlines()
+
+print("\nThese are the names to search for:\n")
+for term in search_terms:
+	print(term.strip(", \n"))
+
+search_term_file.close()
