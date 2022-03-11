6c23a78e6ddc9e5eba6bd099dca7d7dd0673a930
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2022-03-11 14:22:55 -0500

    Added comment: alternate software version
    Still looking into what to use

--- resources/find_terms_in_file.py
+++ resources/find_terms_in_file.py
@@ -1,5 +1,8 @@
 search_term_file = open("search_terms.csv", "r")
 # https://pdfgrep.org/ may be helpful for searching the PDF
+# https://soft.rubypdf.com/software/pdfgrep-windows-version
+# may be a Windows version
+
 # pdf_to_search = open("pdfs/searchable_pdf_000.pdf", "r")
 
 search_terms = search_term_file.readlines()
